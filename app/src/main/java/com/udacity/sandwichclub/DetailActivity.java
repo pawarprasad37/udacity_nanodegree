7ee75c7d8ce044ffc32bb8f95b3300fd904ea929
Prasad Pawar <prasad@Prasads-MacBook-Air.local>
2018-11-04 00:43:40 +0530

    project complte as per the specs.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,27 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        ((TextView) findViewById(R.id.also_known_tv))
+                .setText(getStringFromList(sandwich.getAlsoKnownAs()));
+        ((TextView) findViewById(R.id.ingredients_tv))
+                .setText(getStringFromList(sandwich.getIngredients()));
+        ((TextView) findViewById(R.id.origin_tv))
+                .setText(sandwich.getPlaceOfOrigin());
+        ((TextView) findViewById(R.id.description_tv))
+                .setText(sandwich.getDescription());
+    }
 
+    private String getStringFromList(List<String> list) {
+        if (list == null || list.isEmpty()) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String oneItem : list) {
+            stringBuilder.append("- ");
+            stringBuilder.append(oneItem);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,50 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            Sandwich sandwich = new Sandwich();
+            JSONObject jsonObject = new JSONObject(json);
+
+            JSONObject name = jsonObject.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            sandwich.setMainName(mainName);
+
+            ArrayList<String> alternateNamesList = new ArrayList<>();
+            JSONArray alsoKnownAs = name.getJSONArray("alsoKnownAs");
+            for (int i = 0; i < alsoKnownAs.length(); i++) {
+                alternateNamesList.add(alsoKnownAs.getString(i));
+            }
+            sandwich.setAlsoKnownAs(alternateNamesList);
+
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            sandwich.setPlaceOfOrigin(placeOfOrigin);
+
+            String description = jsonObject.getString("description");
+            sandwich.setDescription(description);
+
+            String imageURL = jsonObject.getString("image");
+            sandwich.setImage(imageURL);
+
+            ArrayList<String> ingredientsList = new ArrayList<>();
+            JSONArray ingredients = jsonObject.getJSONArray("ingredients");
+            for (int i = 0; i < ingredients.length(); i++) {
+                ingredientsList.add(ingredients.getString(i));
+            }
+            sandwich.setIngredients(ingredientsList);
+
+            return sandwich;
+        } catch (JSONException e) {
+            return null;
+        }
     }
 }
